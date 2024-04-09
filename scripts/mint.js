62dfa29286f234a7005f653b273247604ef442f9
ranror <166495159+ranror@users.noreply.github.com>
2024-04-09 19:58:50 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,14 +1,14 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const contractAddress = "0x6B28e97Fd505d759F4dB115C41398D8A384eE3Ce";
-  const receiverAddress = "0x9dB01459c498fB5D18b23d537e1A50A749CeF092";
+  const contractAddress = "0x898Ed94F8821296d60441781b9D55DC2969aF678";
+  const receiverAddress = "0xfb2E423f2d2A519FD94d06c86Cd0E2096459B7Bc";
   const quantity = 2;
   const value = ethers.parseEther("0.03");
   const baseURI =
-    "https://lime-abundant-peafowl-776.mypinata.cloud/ipfs/QmQpWHw1RHCK8Ybax72PFm7gyQvZ3xSarpyoB6WMwD8phV/";
+    "https://plum-important-stork-68.mypinata.cloud/ipfs/QmPwaVQLJYyoSttL6ACipXyJQ7WxVqLSrdM8w3i2Mt1SL3/";
 
-  const batchNFTs = await ethers.getContractAt("BatchNFTs", contractAddress);
+  const batchNFTs = await ethers.getContractAt("myNFTs", contractAddress);
 
   await batchNFTs.setBaseURI(baseURI);
   const mintTokens = await batchNFTs.mint(receiverAddress, quantity, {
