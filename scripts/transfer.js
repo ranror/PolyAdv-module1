638432218bf9d1f765a33787b390635a6eba2c0f
ranror <166495159+ranror@users.noreply.github.com>
2024-04-09 19:59:07 +0530

    Update transfer.js

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -1,26 +1,66 @@
 const { ethers } = require("hardhat");
+const {
+  FXRootContractAbi,
+} = require("/erc721a-implementation/artifacts/FXRootContractAbi.js");
+const ABI = require("../artifacts/contracts/BatchNFTs.sol/myNFTs.json");
+require("dotenv").config();
+try {
+  console.log("Initializing contract instances...");
+  const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+  console.log("Token contract instance created.");
 
-async function main() {
-  const contractAddress = "0x898Ed94F8821296d60441781b9D55DC2969aF678";
-  const receiverAddress = "0xfb2E423f2d2A519FD94d06c86Cd0E2096459B7Bc";
-  const quantity = 2;
-  const value = ethers.parseEther("0.03");
-  const baseURI =
-    "https://plum-important-stork-68.mypinata.cloud/ipfs/QmPwaVQLJYyoSttL6ACipXyJQ7WxVqLSrdM8w3i2Mt1SL3/";
-
-  const batchNFTs = await ethers.getContractAt("myNFTs", contractAddress);
-
-  await batchNFTs.setBaseURI(baseURI);
-  const mintTokens = await batchNFTs.mint(receiverAddress, quantity, {
-    value: value,
-  });
-
-  console.log(
-    `Transaction Hash: https://sepolia.etherscan.io/tx/${mintTokens.hash}`
+  const fxContract = await hre.ethers.getContractAt(
+    fxRootContractABI,
+    fxRootAddress
   );
+  console.log("FxRoot contract instance created.");
+
+  // ... continue with your code
+
+  async function main() {
+    const networkAddress =
+      "https://eth-sepolia.g.alchemy.com/v2/tbG-68_qjk99I8BdZc7uv-ZV87gVlpvp";
+    const privateKey = process.env.PRIVATE_KEY;
+    const provider = new ethers.JsonRpcProvider(networkAddress);
+
+    const wallet = new ethers.Wallet(privateKey, provider);
+
+    const [signer] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("myNFTs");
+    const nft = await NFT.attach("0xcC22949cafD62E5040f7B81EE8cc522669bcC792");
+
+    const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
+    const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
+
+    const tokenIds = [0, 1, 2, 3, 4];
+
+    const approveTx = await nft
+      .connect(signer)
+      .setApprovalForAll(fxRootAddress, true);
+    await approveTx.wait();
+
+
+    for (let i = 0; i < tokenIds; i++) {
+      const depositTx = await fxRoot
+        .connect(signer)
+        .deposit(nft.address, wallet.address, tokenIds[i], "0x6566");
+      await depositTx.wait();
+    }
+
+    console.log("Approved the address and now it is deposited");
+
+    const balance = await nft.balanceOf(wallet.address);
+
+    console.log("wallet balance", wallet.address, "is: ", balance.toString());
+  }
+} catch (error) {
+  console.error("Error initializing contract instances:", error);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
